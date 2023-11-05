9499e3ddeafc9b2d9180bfd85c0843cb9ac2afb4
Bruno Menezes <85026283+brunoleomenezes@users.noreply.github.com>
2023-11-04 21:17:18 -0300

    Create Bike.java
    Exercício Módulo 7

---
+++ Bike.java
@@ -0,0 +1,32 @@
+package java_learning;
+
+/**
+ * @author bruno.menezes
+ * 
+ * 	Make bike class
+ */
+
+public class Bike {
+	
+	// Type bike (classic, sport...)
+	private String tipo;
+	
+	private int ano;
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+}
